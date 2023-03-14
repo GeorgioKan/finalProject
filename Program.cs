5ac1bc932c5b6f245e668478fb3f6ecab2d004a5
Gerorge <logovas4@gmail.com>
2023-03-14 22:07:15 +0300

    Final task added

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿int ElementsCounter(string[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+void StringCreator(string[] array1, string[] array2)
+{
+    int i = 0;
+    for (int j = 0; j < array1.Length; j++)
+    {
+        if (array1[j].Length <= 3)
+        {
+            array2[i] = array1[j];
+            i++;
+        }
+    }
+}
+
+string[] mass = { "1234", "1567", "-2", "computer science", ":-)" };
+int numb = ElementsCounter(mass);
+string[] newMass = new string[numb];
+StringCreator(mass, newMass);
+Console.WriteLine("[" + string.Join(" , ", newMass) + "]");
